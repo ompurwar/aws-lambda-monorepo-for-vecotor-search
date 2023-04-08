435a0c8cb62bcfc53d24f6a670fbf1b961fedbe0
om purwar <kaidranzerom@gmail.com>
2023-04-09 02:28:42 +0530

    initial commit

---
+++ image-service/handler.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Hello from Lambda!',
+      },
+      null,
+      2
+    ),
+  };
+};
